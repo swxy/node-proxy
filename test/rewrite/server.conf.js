c7448bdb8c3204fbd3803d9e1e278657d6836b41
swxy <swxymile@163.com>
2016-09-02 10:20:29 +0800

    static file

--- test/rewrite/server.conf.js
+++ test/rewrite/server.conf.js
@@ -1,12 +1,24 @@
 module.exports = {
-    host: '',
     map: {
-        '/test': 'http://www.tuicool.com'
+        '/test': 'http://www.tuicool.com',
+        '/topics': 'http://www.tuicool.com/topics'
     },
     regex: [
         {
+            match: /^\/assets\//,
+            to: 'http://static0.tuicool.com/'
+        },
+        {
             match: /^\/static\//,
-            to: ''
+            to: function (path) {
+                return '/public' + path;
+            }
+        },
+        {
+            match: /\/sc\/pc\/deal\/getProductTypeAndCategory\?dealId=20940264/,
+            to: function (path) {
+                return 'http://mct.y.nuomi.com' + path;
+            }
         }
     ]
     
